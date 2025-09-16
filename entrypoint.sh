4bfc79ae01ae121a9613d3eaea4a4ff24548bbf3
0jikuji0 <k13joj@proton.me>
2025-09-16 23:29:18 +0200

    edit entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+# Créer la DB SQLite si elle n’existe pas
 if [ ! -f /var/www/html/database/database.sqlite ]; then
     touch /var/www/html/database/database.sqlite
     chown www-data:www-data /var/www/html/database/database.sqlite
     chmod 664 /var/www/html/database/database.sqlite
 fi
 
+# Lancer les migrations
+php artisan migrate --force
+
+# Lancer Apache
 exec apache2-foreground
